22dc0eb44636780af2609be9c5b24499e0cb812c
zhanghaoxin <17310408750@163.com>
2023-11-07 21:13:06 +0800

    更新statusBar

--- src/components/Layout/StatusBar/StatusBar.tsx
+++ src/components/Layout/StatusBar/StatusBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './StatusBar.module.scss';
 import {useStores} from "../../../pkg/store/StoreContext.tsx";
+import {Apple} from "@icon-park/react";
 
 interface StatusBarProps {
 
@@ -11,12 +12,22 @@ const StatusBar: React.FC<StatusBarProps> = () => {
 
     return (
         <div className={styles.statusBar}>
-            <div className={styles.statusBarItem}>{statusBarStore.time}</div>
-            <div className={styles.statusBarItem}>
-                {statusBarStore.wifiConnected ? 'WiFi Connected' : 'No WiFi'}
+            <div className={styles.appContextMenu}>
+                <div className={styles.appStatusLogo}>
+                    <Apple theme="filled" size="18" fill="#fff"/>
+                </div>
+                <div className={styles.appContextMenuName}>WebStorm</div>
+                <div className={styles.appContextMenuItem}>文件</div>
+                <div className={styles.appContextMenuItem}>编辑</div>
             </div>
-            <div className={styles.statusBarItem}>Battery: {statusBarStore.batteryLevel}%</div>
-            {/* 添加更多状态信息... */}
+            <div className={styles.appStatusMenu}>
+                <div className={styles.statusBarItem}>{statusBarStore.time}</div>
+                <div className={styles.statusBarItem}>
+                    {statusBarStore.wifiConnected ? 'WiFi Connected' : 'No WiFi'}
+                </div>
+                <div className={styles.statusBarItem}>Battery: {statusBarStore.batteryLevel}%</div>
+                {/* 添加更多状态信息... */}
+            </div>
         </div>
     );
 };
