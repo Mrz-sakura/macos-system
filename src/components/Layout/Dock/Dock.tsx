0e882a52416a1bff542c74c789b85dda5dcd4289
zhanghaoxin <17310408750@163.com>
2023-11-09 10:35:21 +0800

    增加dock

--- src/components/Layout/Dock/Dock.tsx
+++ src/components/Layout/Dock/Dock.tsx
@@ -1,11 +1,24 @@
 // Dock.tsx
 import React from 'react';
 import styles from './Dock.module.scss';
+import MessagerIcon from '../../../assets/icon/messager.png'
 
 const Dock: React.FC = () => {
     return <div className={styles.dock}>
-        <div>Dock Item 1</div>
-        <div>Dock Item 2</div>
+        <div className={styles.dockWrap}>
+            <div className={styles.dockItem}>
+                <div className={styles.dockItemIcon}>
+                    <img src="https://static.diffusenetwork.com/report/AppIcon%20104.png" alt=""/>
+                </div>
+                {/*<div className={styles.dockItemTitle}>Finder</div>*/}
+            </div>
+            <div className={styles.dockItem}>
+                <div className={styles.dockItemIcon}>
+                    <img src={MessagerIcon} alt=""/>
+                </div>
+                {/*<div className={styles.dockItemTitle}>Finder</div>*/}
+            </div>
+        </div>
     </div>;
 };
 
